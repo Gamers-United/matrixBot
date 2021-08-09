576d25f16f655a9cc37fadaa54b4c2d9a9fe5ccb
MacLim <xmachd@gmail.com>
2021-08-09 19:22:35 +1000

    remove discord_slash requirment

--- bot-v2/bot.py
+++ bot-v2/bot.py
@@ -1,7 +1,6 @@
 import os, json
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
 from sqlalchemy import create_engine, Column, Integer, String, Table, Text, ForeignKey, PickleType
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from types import SimpleNamespace
