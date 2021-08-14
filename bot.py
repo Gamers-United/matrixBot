2b3b816ad98fe8aa85aa5e7478915305d52d5eac
MacLim <xmachd@gmail.com>
2021-08-14 23:54:34 +1000

    add help command

--- bot.py
+++ bot.py
@@ -320,10 +320,17 @@ async def on_command_error(ctx, error):
     else:
         print(error)
 
-#doesn't work new patch necessary.
-#@bot.command(name=".")
-#async def preventresponse(ctx):
-#    return
+#help command
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="Matrix Help", description="These are all the commands available, with Matrix.", color=0x00ff00)
+    embed.add_field(name="Community | Status", value="newcommunity 'name', deletecommunity 'name'", inline=False)
+    embed.add_field(name="Community | Admin", value="invite 'community name' 'member', kick 'member' 'community name', promote 'community name' 'member', demote 'community name' 'member'", inline=False)
+    embed.add_field(name="Community | User", value="accept 'community name', leave 'community name'", inline=False)
+    embed.add_field(name="Voice | Admin", value="voice setup 'channel id' 'category id', voice setlimit 'number'", inline=False)
+    embed.add_field(name="Voice | User", value="voice lock, voice unlock, voice permit 'member', voice reject 'member', voice limit 'number', voice name 'name', voice claim, voice ghost, voice unghost", inline=False)
+    embed.add_field(name="Development", value="shutdown, reload", inline=False)
+    await ctx.send(embed=embed)
 
 #run the bot
 try:
