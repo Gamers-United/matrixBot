6fe72b2b28b322d4ae043e5f043b1b5cc716fd3c
MacLim <xmachd@gmail.com>
2021-09-15 21:04:24 +1000

    fix missing right bracket

--- music.py
+++ music.py
@@ -61,7 +61,7 @@ class Music(commands.Cog):
             await guild.change_voice_state(channel=None)
 
     @commands.command(aliases=['p'])
-    async def play(self, ctx, *, query: str, search_type:str = "yt":
+    async def play(self, ctx, *, query: str, search_type:str = "yt"):
         """ Searches and plays a song from a given query. """
         player = self.bot.lavalink.player_manager.get(ctx.guild.id)
         #move on to query
