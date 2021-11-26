25e67dabbbda55e66b13143ecd749ee009e5a417
MacLim <xmachd@gmail.com>
2021-11-26 23:27:33 +1100

    Fix missing ctx args in random

--- randomresults.py
+++ randomresults.py
@@ -5,19 +5,19 @@ class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
-    async def roll(self, request:str):
+    async def roll(self, ctx, request:str):
         embed = discord.Embed(title="Roll Result", description="Need help with the syntax? View the docs: https://d20.readthedocs.io/en/latest/start.html#dice-syntax")
         roll = d20.roll(request)
         embed.add_field("Results", str(roll))
         await ctx.send(embed=embed)
     @commands.command()
-    async def reorder(self, request:str):
+    async def reorder(self, ctx, request:str):
         values = request.split(',')
         random.shuffle(values)
         embed = discord.Embed(title="Reordered Result", description=str(values))
         await ctx.send(embed=embed)
     @commands.command()
-    async def pick(self, request:str):
+    async def pick(self, ctx, request:str):
         values = request.split(',')
         values = random.choice(values)
         embed = discord.Embed(title="Picked Result", description=str(values))
