9a4bb0da4021e0fe27a53a8d82ad048f4af4c02f
MacLim <xmachd@gmail.com>
2021-11-27 23:01:10 +1100

    handle all exceptions within the cog

--- randomresults.py
+++ randomresults.py
@@ -4,18 +4,24 @@ from discord.ext import commands
 class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    async def cog_command_error(self, ctx, error):
+        await ctx.send("An error has occured!\n"+str(error))
+
     @commands.command()
     async def roll(self, ctx, request:str):
         roll = d20.roll(request)
         embed = discord.Embed(title="Roll Result", description=str(roll))
         embed.add_field(name="Need Help?", value="View the docs: https://d20.readthedocs.io/en/latest/start.html#dice-syntax")
         await ctx.send(embed=embed)
+
     @commands.command()
     async def reorder(self, ctx, request:str):
         values = request.strip().split(',')
         random.shuffle(values)
         embed = discord.Embed(title="Reordered Result", description=str(values))
         await ctx.send(embed=embed)
+        
     @commands.command()
     async def pick(self, ctx, request:str):
         values = request.strip().split(',')
