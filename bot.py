ba698f7f92d821b8283ea7db57ad816efae16ae2
mac <mac@pop-os.localdomain>
2022-04-11 20:55:34 +1000

    fix some voice state stuff and update help command

--- bot.py
+++ bot.py
@@ -68,39 +68,57 @@ async def on_command_error(ctx, error):
 
 #bot logging
 @bot.event
-async def on_voice_state_update(member, before, after):
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     timenow = datetime.now()
-    embed=discord.Embed(title="New Voice State", description=str(member.name)+" "+str(timenow))
-    embed.add_field(name="AFK", value=after.afk, inline=False)
-    try:
-        embed.add_field(name="Channel", value=str(after.channel.id)+"\n"+after.channel.category.name+" | "+after.channel.guild.name+" | "+after.channel.name, inline=False)
-    except AttributeError:
-        embed.add_field(name="Channel", value="N/A", inline=False)
-    embed.add_field(name="Deafened", value=after.deaf, inline=False)
-    embed.add_field(name="Muted", value=after.mute, inline=False)
-    embed.add_field(name="Requested To Speak Timestamp", value=after.requested_to_speak_at, inline=False)
-    embed.add_field(name="Self Deafened", value=after.self_deaf, inline=False)
-    embed.add_field(name="Self Muted", value=after.self_mute, inline=False)
-    embed.add_field(name="Streaming", value=after.self_stream, inline=False)
-    embed.add_field(name="Video", value=after.self_video, inline=False)
-    embed.add_field(name="Supressed", value=after.suppress, inline=False)
+    embed=discord.Embed(title=f"{member.name}", description=str(member.name)+" "+str(timenow))
+    if not before.channel and after.channel is not None:
+        embed.add_field(name="Joined", value=f"{after.channel.name}/{after.channel.category.name}/{after.channel.guild.name}")
+    elif before.channel is not None and not after.channel:
+        embed.add_field(name="Left", value=f"{before.channel.name}/{before.channel.category.name}/{before.channel.guild.name}")
+    elif after.afk and before.channel is not None:
+        embed.add_field(name="Went AFK", value=f"{before.channel.guild.name}")
+    elif not before.self_deaf and after.self_deaf:
+        embed.add_field(name="Deafened Themselves", value=f"{before.channel.guild.name}")
+    elif before.self_deaf and not after.self_deaf:
+        embed.add_field(name="Undeafened Themselves", value=f"{before.channel.guild.name}")
+    elif not before.self_mute and after.self_mute:
+        embed.add_field(name="Muted Themselves", value=f"{before.channel.guild.name}")
+    elif before.self_mute and not after.self_mute:
+        embed.add_field(name="Unmuted Themselves", value=f"{before.channel.guild.name}")
+    elif not before.self_video and after.self_video:
+        embed.add_field(name="Has Begun Streaming Video", value=f"{before.channel.guild.name}")
+    elif before.self_video and not after.self_video:
+        embed.add_field(name="Has Ended Their Video Streaming", value=f"{before.channel.guild.name}")
+    elif not before.mute and after.mute:
+        embed.add_field(name="Was Muted By An Admin", value=f"{before.channel.guild.name}")
+    elif before.mute and not after.mute:
+        embed.add_field(name="Was Unmuted by an Admin", value=f"{before.channel.guild.name}")
+    elif not before.deaf and after.deaf:
+        embed.add_field(name="Was Deafened By An Admin", value=f"{before.channel.guild.name}")
+    elif before.deaf and not after.deaf:
+        embed.add_field(name="Was Undeafened By An Admin", value=f"{before.channel.guild.name}")
     await log["VOICE"].send(embed=embed)
 
 #help command
 @bot.command()
 async def help(ctx):
     embed=discord.Embed(title="Matrix Help", description="""These are all the commands available, with Matrix.""", color=0x00ff00)
-    embed.add_field(name="Music | Basic", value="""play 'link or name'
+    embed.add_field(name="Music | Basic", value="""play 'spotify link, youtube (music) link, sound cloud link, mp3 link or name'
+    connect 'channel'
+    disconnect, stop, leave, dc
     pause
     resume
-    nowplaying (np)
-    seek
-    queue
-    remove
-    skip""", inline=False)
-    embed.add_field(name="Music | Advanced", value="""volume 'level'
-    eq 'band 0-14' 'level -0.25 to 1.0'
-    reseteq""", inline=False)
+    nowplaying, np 
+    seek 'time' (Minutes and/or seconds)
+    shuffle (current queue)
+    repeat (toggle)
+    queue 'page'
+    lyrics (for the current song)
+    remove 'number in queue to remove'
+    skip 'number to skip (default of 1)'""", inline=False)
+    embed.add_field(name="Music | Advanced", value="""volume 'level' (0 to 500%)
+    setFilter (GUI Command)
+    deleteFilter (GUI Command)""", inline=False)
     embed.add_field(name="Fun | Jokes", value="""joke
     dadjoke
     copypasta""", inline=False)
