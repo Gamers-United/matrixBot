0c5d66d041288c88449b611cea02c29c4f592536
Mac <mac@pop-os.localdomain>
2022-04-11 11:24:03 +1000

    reinitializate player after invoking connect in case it is now connected

--- music.py
+++ music.py
@@ -99,6 +99,7 @@ class Music(commands.Cog):
         player: CustomPlayer = ctx.voice_client
         if not player:
             await ctx.invoke(self.connect)
+        player: CustomPlayer = ctx.voice_client
         if not player:
             #since we just tried to join, if it failed to join, then the person must not be in a accessible VC.
             await ctx.send("You are not in a voice channel.")
