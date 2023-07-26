d175e60e7e004cf4678be425204a383c37807c5e
Mac898 <xmachd@gmail.com>
2023-07-27 01:56:25 +1000

    continue solving issues

--- minecraft_smp.py
+++ minecraft_smp.py
@@ -50,7 +50,7 @@ class MinecraftSMP:
         with Session(self.db) as session:
             message = session.get(MinecraftSMPServers, 1)
             msg = await self.bot.get_channel(message.channel_id).fetch_message(message.message_id)
-            await msg.edit(embed=self.generate_embed())
+            await msg.edit(embed=self.generate_embed(), content="")
 
     def new_user(self, uuid, name):
         with Session(self.db) as session:
