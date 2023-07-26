b5eb8db64858ee9a0f19f399624e32f27102e6c0
Mac898 <xmachd@gmail.com>
2023-07-27 03:25:13 +1000

    update

--- games.py
+++ games.py
@@ -3,6 +3,7 @@
 #  A copy of this license should have been provided with the code download, if not see https://www.gnu.org/licenses/
 #  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 #  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU AGPL v3.0 for more details.
+import json
 import multiprocessing
 import os
 import re
